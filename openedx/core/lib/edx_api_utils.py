523da3173226510ba0302fe02b8bbef0b64a8624
Renzo Lucioni <renzo@renzolucioni.com>
2016-05-25 12:46:56 -0400

    WIP: Extend API helper to support lookup by ID

--- openedx/core/lib/edx_api_utils.py
+++ openedx/core/lib/edx_api_utils.py
@@ -11,7 +11,7 @@ from openedx.core.lib.token_utils import get_id_token
 log = logging.getLogger(__name__)
 
 
-def get_edx_api_data(api_config, user, resource, querystring=None, cache_key=None):
+def get_edx_api_data(api_config, user, resource, resource_id=None, querystring=None, cache_key=None):
     """Fetch data from an API using provided API configuration and resource
         name.
 
@@ -21,6 +21,9 @@ def get_edx_api_data(api_config, user, resource, querystring=None, cache_key=Non
         user (User): The user to authenticate as when requesting data.
         resource(str): Name of the API resource for which data is being
             requested.
+
+    Keyword Arguments:
+        resource_id (int or str): Identifies the specific object to be retrieved.
         querystring(dict): Querystring parameters that might be required to
             request data.
         cache_key(str): Where to cache retrieved data. Omitting this will cause the
@@ -36,8 +39,10 @@ def get_edx_api_data(api_config, user, resource, querystring=None, cache_key=Non
         return no_data
 
     if cache_key:
+        cache_key = '{}.{}'.format(cache_key, resource_id) if resource_id else cache_key
+
         cached = cache.get(cache_key)
-        if cached is not None:
+        if cached:
             return cached
 
     try:
@@ -49,16 +54,21 @@ def get_edx_api_data(api_config, user, resource, querystring=None, cache_key=Non
 
     try:
         querystring = {} if not querystring else querystring
-        response = getattr(api, resource).get(**querystring)
-        results = response.get('results', no_data)
-        page = 1
-        next_page = response.get('next', None)
-        while next_page:
-            page += 1
-            querystring['page'] = page
-            response = getattr(api, resource).get(**querystring)
-            results += response.get('results', no_data)
-            next_page = response.get('next', None)
+        response = getattr(api, resource)(resource_id).get(**querystring)
+
+        if resource_id:
+            results = response
+        else:
+            results = response.get('results', no_data)
+
+            page = 1
+            next_page = response.get('next')
+            while next_page:
+                page += 1
+                querystring['page'] = page
+                response = getattr(api, resource).get(**querystring)
+                results += response.get('results', no_data)
+                next_page = response.get('next')
     except Exception:  # pylint: disable=broad-except
         log.exception('Failed to retrieve data from the %s API.', api_config.API_NAME)
         return no_data
